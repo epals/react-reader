3e6b9e3ce9afe73453f01c426bc2e556a1b5eec0
Gerhard Sletten <gerhardsletten@gmail.com>
2020-08-04 10:45:39 +0200

    gh-pages update

---
+++ precache-manifest.5711eb9e1a7df907004bcc2eae5fd298.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b1acf66d013df758dbccf468b851412",
+    "url": "/react-reader/index.html"
+  },
+  {
+    "revision": "e98d020414cf53bb163b",
+    "url": "/react-reader/static/js/2.4eb57029.chunk.js"
+  },
+  {
+    "revision": "ee9d1d01d076f8eb8905629ccc10e982",
+    "url": "/react-reader/static/js/2.4eb57029.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aee96211bd596f37d7f6",
+    "url": "/react-reader/static/js/main.7279359d.chunk.js"
+  },
+  {
+    "revision": "ff36ed2267d8f58fd59d",
+    "url": "/react-reader/static/js/runtime-main.4440aa88.js"
+  }
+]);
